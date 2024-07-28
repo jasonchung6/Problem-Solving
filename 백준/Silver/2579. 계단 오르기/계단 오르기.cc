58ac9247ff7bd2cfd8c583c1f31a0253e0566f24
Jaeseung Jason Chung <124449788+jasonchung6@users.noreply.github.com>
2024-07-29 02:56:18 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<int> vec(n, 0);
+	vector<int> dp(n, 0);
+	for (int i = 0; i < n; ++i)
+		cin >> vec[i];
+
+	dp[0] = vec[0];
+	dp[1] = vec[0] + vec[1];
+	dp[2] = max(vec[0]+vec[2],vec[1]+vec[2]);
+
+	for (int i = 3; i < n; ++i)
+	{
+		dp[i] = max(dp[i - 2] + vec[i], vec[i - 1]+vec[i] + dp[i-3]);
+	}
+
+
+	cout << dp[n-1];
+
+	return 0;
+}
