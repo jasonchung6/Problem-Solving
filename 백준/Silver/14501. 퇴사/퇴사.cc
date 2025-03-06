65944b64e11a50b2efb2051b6d1562c94f1588bd
Jaeseung Jason Chung <124449788+jasonchung6@users.noreply.github.com>
2025-03-07 02:30:07 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	int DP[16] = { 0, };
+	int TP[16][2] = { 0, };
+
+	for (int i = 1; i <= N; ++i)
+	{
+		cin >> TP[i][0] >> TP[i][1];
+	}
+	
+	for (int i = N; i > 0; --i)
+	{
+		int deadline = TP[i][0] + i - 1;
+		if (deadline <= N)
+		{
+			DP[i] = max(DP[i + 1], (DP[deadline + 1] + TP[i][1]));
+		}
+		else
+		{
+			DP[i] = DP[i + 1];
+		}
+	}
+
+	cout << DP[1];
+
+	return 0;
+}
