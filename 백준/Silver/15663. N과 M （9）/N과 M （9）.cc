1e54b6940e3d633f402fa4c2ebbc0161c18cd029
Jaeseung Jason Chung <124449788+jasonchung6@users.noreply.github.com>
2025-04-06 02:00:02 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+
+void backtrack(int dis, vector<int>& combination, vector<pair<int, int>>& numbers)
+{
+	if (combination.size() == M)
+	{
+		for (const int& num : combination)
+		{
+			cout << num << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	int check = 0;
+	for (auto it = numbers.begin(); it != numbers.end(); ++it)
+	{
+		if ((*it).second == 0 && check != (*it).first)
+		{
+			combination.push_back((*it).first);
+			(*it).second = 1;
+			check = (*it).first;
+			backtrack(distance(numbers.begin(), it + 1), combination, numbers);
+			combination.pop_back();
+			(*it).second = 0;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+
+	vector<int> combination;
+	vector<pair<int, int>> numbers(N, { 0, 0 });
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> numbers[i].first;
+	}
+	sort(numbers.begin(), numbers.end());
+
+	backtrack(0, combination, numbers);
+
+	return 0;
+}
