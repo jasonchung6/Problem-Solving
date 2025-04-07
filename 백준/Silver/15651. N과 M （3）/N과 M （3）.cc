08cb372a6a9e0dfc64810c79fe0354d75f8b5d08
Jaeseung Jason Chung <124449788+jasonchung6@users.noreply.github.com>
2025-04-07 23:10:18 +0900

    [Silver III] Title: N과 M (3), Time: 332 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+
+void backtrack(vector<int>& combination)
+{
+	if (combination.size() == M)
+	{
+		for (const int& num : combination)
+		{
+			cout << num << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= N; ++i)
+	{
+		combination.push_back(i);
+		backtrack(combination);
+		combination.pop_back();
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+
+	vector<int> combination;
+	backtrack(combination);
+
+	return 0;
+}
