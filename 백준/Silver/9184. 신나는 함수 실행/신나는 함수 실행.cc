7b79ffd4460ac0769b6b6871d5ac2b1f5956d7f0
Jaeseung Jason Chung <124449788+jasonchung6@users.noreply.github.com>
2025-08-10 02:12:20 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int dp[21][21][21] = { 0, };
+
+int solve(int a, int b, int c)
+{
+	if (a <= 0 || b <= 0 || c <= 0)
+	{
+		return 1;
+	}
+	else if (a > 20 || b > 20 || c > 20)
+	{
+		if (dp[20][20][20] != 0)
+		{
+			return dp[20][20][20];
+		}
+		else
+		{
+			return dp[20][20][20] = solve(20, 20, 20);
+		}
+	}
+	else if (dp[a][b][c] != 0)
+	{
+		return dp[a][b][c];
+	}
+	else if (a < b && b < c)
+	{
+		dp[a][b][c - 1] = solve(a, b, c - 1);
+		dp[a][b - 1][c - 1] = solve(a, b - 1, c - 1);
+		dp[a][b - 1][c] = solve(a, b - 1, c);
+		return dp[a][b][c - 1] + dp[a][b - 1][c - 1] - dp[a][b - 1][c];
+	}
+	else
+	{
+		dp[a - 1][b][c] = solve(a - 1, b, c);
+		dp[a - 1][b - 1][c] = solve(a - 1, b - 1, c);
+		dp[a - 1][b][c - 1] = solve(a - 1, b, c - 1);
+		dp[a - 1][b - 1][c - 1] = solve(a - 1, b - 1, c - 1);
+		return dp[a - 1][b][c] + dp[a - 1][b - 1][c] + dp[a - 1][b][c - 1] - dp[a - 1][b - 1][c - 1];
+
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); 
+
+	int a, b, c;
+	int ans = 0;
+	
+	while (1)
+	{
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1)
+		{
+			break;
+		}
+
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << solve(a,b,c) << '\n';
+	}
+
+ 	return 0;
+}
